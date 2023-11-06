08c05dde956f2b014244f6f3045dbd1f1f932fed
yeongyurim <zxc991019@naver.com>
2023-11-06 16:42:07 +0900

    aa

---
+++ 100joon/Condition Variation/prob2884.c
@@ -0,0 +1,11 @@
+// prob2884.c
+// 45 min earlier alarm
+
+#include <stdio.h>
+
+int main(void) {
+    unsigned short hour, min;
+    scanf("%hu %hu", &hour, &min); // %hu == unsigned short
+    
+    
+}
