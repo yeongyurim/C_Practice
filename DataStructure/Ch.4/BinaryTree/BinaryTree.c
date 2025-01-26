2908c2ceaf42c5d6160e783b54749e67beb7a145
yeongyurim <zxc991019@naver.com>
2025-01-26 17:54:36 +0900

    qwdqwd

--- DataStructure/Ch.4/BinaryTree/BinaryTree.c
+++ DataStructure/Ch.4/BinaryTree/BinaryTree.c
@@ -29,3 +29,48 @@ void SBT_DestroyTree( SBTNode* Node )
     // 뿌리 노드 소멸
     SBT_DestroyTree( Node );
 }
+
+void SBT_PreorderPrintTree( SBTNode* Node )
+{
+    if( Node == NULL )
+        return;
+
+    // 뿌리 노드 출력
+    printf( " %c", Node->Data);
+
+    // 왼쪽 하위 트리 출력
+    SBT_PreorderPrintTree( Node->Left );
+
+    // 오른쪽 하위 트리 출력
+    SBT_PreorderPrintTree( Node->Right );
+}
+
+void SBT_InorderPrintTree( SBTNode* Node )
+{
+    if( Node == NULL )
+        return;
+    
+    // 왼쪽 하위 트리 출력
+    SBT_InorderPrintTree( Node->Left );
+
+    // 뿌리 노드 출력
+    printf( " %c", Node->Data );
+
+    // 오른쪽 하위 트리 출력
+    SBT_InorderPrintTree( Node->Right );
+}
+
+void SBT_PostorderPrintTree( SBTNode* Node )
+{
+    if( Node == NULL )
+        return;
+
+    // 왼쪽 하위 트리 출력
+    SBT_PostorderPrintTree( Node->Left );
+
+    // 오른쪽 하위 트리 출력
+    SBT_PostorderPrintTree( Node->Right );
+
+    // 뿌리 노드 출력
+    printf( " %c", Node->Data );
+}
---
+++ DataStructure/Ch.4/DisjointSet/Test_DisjointSet.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "DisjointSet.h"
+
+int main ( void )
+{
+    int a=1, b=2, c=3, d=4;
+    DisjointSet* Set1
+}
