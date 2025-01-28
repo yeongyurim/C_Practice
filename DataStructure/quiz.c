93f5f2ab129d6515141f5136ce337ae869e111f1
Gyurim Yeon <zxc991019@naver.com>
2025-01-28 09:50:19 +0900

    qwd

--- DataStructure/quiz.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void quiz(){
-    int sizeInt = 10;
-    int* sizePointer = &sizeInt;
-    printf("sizeof(int) : %d \nsizeof(int*) %d\n",sizeof(sizeInt),sizeof(sizePointer));
-};
